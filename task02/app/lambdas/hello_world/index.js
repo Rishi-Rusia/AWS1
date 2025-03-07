eb0f1a3099c11efe76c995bffd1b7c145f2c0582
Rishi Rusia <rishi_rusia@epam.com>
2025-03-07 14:12:36 +0530

    task2y

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,8 +1,18 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        message: 'Hello from Lambda',
-    };
-    return response;
+    const path = event.requestContext.http.path;
+    const method = event.requestContext.http.method;
+
+    if (path === '/hello' && method === 'GET') {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello from Lambda' }),
+        };
+    } else {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+            }),
+        };
+    }
 };
